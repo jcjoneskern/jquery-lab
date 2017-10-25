f1d037b52f9f6a5850573cb5830c572fdcdbc4b2
J-C <jcjoneskern@gmail.com>
2017-10-24 23:09:54 -0400

    pt1

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 $(function() {
 
+  var currentTable;
+
   for(var i = 1; i < 10; i++) {
     $("ul").append("<li class='available'>" + i + "</li>")
   }
 
   $(".available").on("click", function() {
     $("form").removeClass("hidden");
+    currentTable = $(this);
+  });
+
+  $("button[name=reserve]").on("click", function(e) {
+    currentTable.removeClass("available");
+    currentTable.addClass("reserved");
+
+    hide(e);
   });
 
   $("button[type=button]").on("click", hide);
-  $("button[name=reserve]").on("click", hide);
-
-
-
 
   function hide(e) {
     e.preventDefault();
