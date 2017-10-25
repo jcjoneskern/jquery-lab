bc23abf1ea9ee81627d9b9bc98a476d9c557c600
J-C <jcjoneskern@gmail.com>
2017-10-24 23:00:29 -0400

    hide/show form

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ $(function() {
     $("ul").append("<li class='available'>" + i + "</li>")
   }
 
+  $(".available").on("click", function() {
+    $("form").removeClass("hidden");
+  });
+
+  $("button[type=button]").on("click", hide);
+  $("button[name=reserve]").on("click", hide);
+
+
+
+
+  function hide(e) {
+    e.preventDefault();
+    $("form").addClass("hidden");
+  }
+
 });
